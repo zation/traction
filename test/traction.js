36040d60ead354d124fb35a1d22fe3ec0cb33561
Liu Yang <zation1@gmail.com>
2015-04-08 14:54:06 +0800

    merge object to array

--- test/traction.js
+++ test/traction.js
@@ -5,9 +5,33 @@ chai.should();
 
 var utils = require('../lib/traction');
 
-describe.only('utils', function() {
-  it('should merge array based on id', function() {
-    var newData = [{
+describe('utils', function() {
+  describe('should merge object to array based on id', function() {
+    it('when id is existed', function() {
+      var object = {
+        id: 1,
+        name: 'liu'
+      };
+      var array = [{
+        id: 1,
+        name: 'yang'
+      }, {
+        id: 2,
+        name: 'qiu'
+      }];
+
+      utils.merge(object).to(array).basedOn('id').should.eql([{
+        id: 1,
+        name: 'liu'
+      }, {
+        id: 2,
+        name: 'qiu'
+      }]);
+    });
+  });
+
+  it('should merge array to array based on id', function() {
+    var newArray = [{
       id: 1,
       name: 'liu',
       email: 'liu@gmail.com',
@@ -17,7 +41,7 @@ describe.only('utils', function() {
       name: 'jex',
       email: 'jex@gmail.com'
     }];
-    var oldData = [{
+    var oldArray = [{
       id: 1,
       name: 'yang',
       email: 'yang@gmail.com'
@@ -27,7 +51,7 @@ describe.only('utils', function() {
       email: 'qiu@gmail.com'
     }];
 
-    utils.merge(newData).to(oldData).basedOn('id').should.eql([{
+    utils.merge(newArray).to(oldArray).basedOn('id').should.eql([{
       id: 1,
       name: 'liu',
       email: 'liu@gmail.com',
